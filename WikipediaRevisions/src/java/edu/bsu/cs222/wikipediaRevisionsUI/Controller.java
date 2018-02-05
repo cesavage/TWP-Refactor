997f509163564db9c4733e174b9d817c09ec2bb7
Chris Savage <cesavage@bsu.edu>
2018-02-05 07:21:59 -0500

    Add JavaFX and Maven framework support. Add ParserTest class from "Parsing JSON using GSON" video. Begin simple UI from "JavaFX with SceneBuilder" video (Ending at 4:00.)

---
+++ WikipediaRevisions/src/java/edu/bsu/cs222/wikipediaRevisionsUI/Controller.java
@@ -0,0 +1,4 @@
+package edu.bsu.cs222.wikipediaRevisionsUI;
+
+public class Controller {
+}
---
+++ WikipediaRevisions/test/java/edu/bsu/cs222/testWikipediaRevisions/TestWikipediaRevisions.java
@@ -0,0 +1,4 @@
+package edu.bsu.cs222.testWikipediaRevisions;
+
+public class TestWikipediaRevisions {
+}
