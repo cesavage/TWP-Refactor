124f0993e9b14c6f8551c1f85428d32c89f129ad
Chris Savage <cesavage@bsu.edu>
2018-02-05 20:27:04 -0500

    Add UI elements up to 21:40 in the tutorial video.
    Includes textfield for URL, button, and code in Controller.java to read HTML from a submitted URL.

--- WikipediaRevisions/src/java/edu/bsu/cs222/wikipediaRevisionsUI/Controller.java
+++ WikipediaRevisions/src/java/edu/bsu/cs222/wikipediaRevisionsUI/Controller.java
@@ -1,4 +1,53 @@
 package edu.bsu.cs222.wikipediaRevisionsUI;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 public class Controller {
+
+    @FXML
+    private TextField urlField;
+
+    private final Executor executor = Executors.newSingleThreadExecutor();
+
+
+    @SuppressWarnings("unused")
+    @FXML
+    public void countWords(ActionEvent actionEvent) {
+        try {
+            final URL url = new URL(urlField.getText());
+            executor.execute(new Runnable() {
+
+                public void run() {
+                   // anonymous inner class
+
+                    try {
+                        BufferedReader reader;
+                        reader = new BufferedReader(new InputStreamReader(url.openStream()));
+                        String line;
+                        while ((line = reader.readLine()) != null){
+                            System.out.println(line);
+
+                        }
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                }
+            });
+
+
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
